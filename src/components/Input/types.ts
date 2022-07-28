815f8e7613aaaf8a8daa1b8d583faaeeecab7773
itsaladin <alauddin12340@gmail.com>
2022-07-29 04:04:41 +0600

    feat: component Input creted

---
+++ src/components/Input/types.ts
@@ -0,0 +1,10 @@
+interface props {
+  style?: any;
+  rest?: any;
+  placeholder?: string;
+  onPressLeftIcon?: any;
+  onPressRightIcon?: any;
+  leftIcon?: string;
+  rightIcon?: string;
+  fontSize?: number;
+}

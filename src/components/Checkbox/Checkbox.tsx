b653d1189e37d36c8cd2814e8944589c42fba556
Arafat Zahan <arafat.zahan@10up.com>
2022-08-02 02:55:08 +0600

    feat: add checkbox component

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,25 @@
+import React, { useCallback } from 'react';
+import type { FC } from 'react';
+import CheckboxContext from './context';
+import type { CheckboxProps, CheckboxValue } from './types';
+
+const Checkbox: FC<CheckboxProps> = ({ onChange, values, children }) => {
+  const handleChange = useCallback(
+    (value: CheckboxValue) => {
+      const newValues = values.includes(value)
+        ? values.filter((v) => v !== value)
+        : [...values, value];
+
+      onChange(newValues);
+    },
+    [values]
+  );
+
+  return (
+    <CheckboxContext.Provider value={{ values, handleChange }}>
+      {children}
+    </CheckboxContext.Provider>
+  );
+};
+
+export default Checkbox;

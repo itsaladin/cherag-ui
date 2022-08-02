39dde551bc0b35833067edac22bef74e5bad14e1
Arafat Zahan <arafat.zahan@10up.com>
2022-08-03 00:29:24 +0600

    fix: fix font loading

--- example/Application.tsx
+++ example/Application.tsx
@@ -1,4 +1,20 @@
 import { loadAsync } from 'expo-font';
+import {
+  AntDesign,
+  Entypo,
+  EvilIcons,
+  Feather,
+  FontAwesome,
+  FontAwesome5,
+  Fontisto,
+  Foundation,
+  Ionicons,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  Octicons,
+  SimpleLineIcons,
+  Zocial,
+} from '@expo/vector-icons';
 import React, { useEffect, useState } from 'react';
 import App from './src/App';
 
@@ -6,10 +22,35 @@ export default function Application() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    loadAsync({
-      'material-community': require('@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf'),
-    }).then(() => setIsLoading(false));
+    (async () => {
+      try {
+        await loadAsync({
+          ...AntDesign.font,
+          ...Entypo.font,
+          ...EvilIcons.font,
+          ...Feather.font,
+          ...FontAwesome.font,
+          ...FontAwesome5.font,
+          ...Fontisto.font,
+          ...Foundation.font,
+          ...Ionicons.font,
+          ...MaterialCommunityIcons.font,
+          ...MaterialIcons.font,
+          ...Octicons.font,
+          ...SimpleLineIcons.font,
+          ...Zocial.font,
+        });
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
   });
+
+  if (isLoading) {
+    return null;
+  }
 
   return <App />;
 }

10a86dd02fc74cc8cdd57ae3c9bae60be4b55351
itsaladin <alauddin12340@gmail.com>
2022-08-10 02:49:17 +0600

    refactor: fix Loader component types

--- src/components/Loader/types.ts
+++ src/components/Loader/types.ts
@@ -1,6 +1,6 @@
-import { StyleProp, ViewProps, ViewStyle } from 'react-native';
+import { ActivityIndicatorProps, StyleProp, ViewStyle } from 'react-native';
 
-export interface LoaderProps extends ViewProps {
+export interface LoaderProps extends ActivityIndicatorProps {
   style?: StyleProp<ViewStyle>;
   size?: number | 'small' | 'large';
   color?: string;

0a2a5781b27059346dbe2e733b535bedc878e528
itsaladin <alauddin12340@gmail.com>
2022-08-10 00:58:55 +0600

    refactor: add types in Button component

---
+++ src/components/Button/types.ts
@@ -0,0 +1,19 @@
+import {
+  GestureResponderEvent,
+  StyleProp,
+  ViewProps,
+  ViewStyle,
+} from 'react-native';
+
+export interface Props extends ViewProps {
+  style?: StyleProp<ViewStyle>;
+  onPress: (event: GestureResponderEvent) => void;
+  width?: number;
+  height?: number;
+  bg?: string;
+  color?: string;
+  fontSize?: number;
+  borderRadius?: number;
+  borderWidth?: number;
+  fontWeight?: string;
+}

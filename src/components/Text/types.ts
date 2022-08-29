304b8445769ff955c115378fd1a815e0022392b3
Alauddin Ahmed <alauddin12340@gmail.com>
2022-08-30 04:08:23 +0600

    refactor: change import name of RNText

--- src/components/Text/types.ts
+++ src/components/Text/types.ts
@@ -1,12 +1,12 @@
 import type {
   StyleProp,
-  TextProps as NativeTextProps,
+  TextProps as RNTextProps,
   TextStyle,
 } from 'react-native';
 
 export type fontSizeProps = 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl';
 
-export interface TextProps extends NativeTextProps {
+export interface TextProps extends RNTextProps {
   children?: React.ReactNode;
   style?: StyleProp<TextStyle>;
   fontSize?: fontSizeProps;

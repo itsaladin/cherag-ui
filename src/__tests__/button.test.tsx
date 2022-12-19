597550c7e9c87bb3dbd3df42b2b5bf6796fd7a4d
aladdin <alauddin12340@gmail.com>
2022-12-20 00:39:50 +0600

    feat: add testId props for button component

--- src/__tests__/button.test.tsx
+++ src/__tests__/button.test.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { cleanup, render, fireEvent } from '@testing-library/react-native';
 import { Button } from '../components/Button';
 
+const testIdName = 'testButtonId';
 afterEach(cleanup);
 
 describe('Button component', () => {
   it('should find the button component via testId', () => {
-    const testIdName = 'testButtonId';
-    const { getByTestId } = render(<Button />);
+    const { getByTestId } = render(<Button testID={testIdName} />);
     const foundButton = getByTestId(testIdName);
     expect(foundButton).toBeTruthy();
   });
 
   it('should call onPress function when pressed', () => {
-    const testIdName = 'testButtonId';
     const onPress = jest.fn();
-    const { getByTestId } = render(<Button onPress={onPress} />);
+    const { getByTestId } = render(
+      <Button onPress={onPress} testID={testIdName} />
+    );
     fireEvent.press(getByTestId(testIdName));
     expect(onPress).toHaveBeenCalledTimes(1);
   });
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -16,13 +16,14 @@ const Button = ({
   borderRadius,
   borderWidth,
   fontWeight,
+  testID,
   ...rest
 }: ButtonProps) => {
   const __center = 'center';
   return (
     <TouchableOpacity
       activeOpacity={0.8}
-      testID={'testButtonId'}
+      testID={testID || 'testButtonId'}
       style={[
         style,
         {

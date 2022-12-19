1e9ce8d6c5ac49f832081e3fc67710a7d7705bed
aladdin <alauddin12340@gmail.com>
2022-12-19 23:42:10 +0600

    feat: create test for button component

--- src/__tests__/button.test.tsx
+++ src/__tests__/button.test.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { cleanup, render } from '@testing-library/react-native';
+import { cleanup, render, fireEvent } from '@testing-library/react-native';
 import { Button } from '../components/Button';
 
 afterEach(cleanup);
 
-describe('App', () => {
+describe('Button component', () => {
   it('should find the button component via testId', () => {
     const testIdName = 'testButtonId';
-
     const { getByTestId } = render(<Button />);
-
     const foundButton = getByTestId(testIdName);
-
     expect(foundButton).toBeTruthy();
+  });
+
+  it('should call onPress function when pressed', () => {
+    const testIdName = 'testButtonId';
+    const onPress = jest.fn();
+    const { getByTestId } = render(<Button onPress={onPress} />);
+    fireEvent.press(getByTestId(testIdName));
+    expect(onPress).toHaveBeenCalledTimes(1);
   });
 });
